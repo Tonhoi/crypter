bbf9c0b2fcbb98efd208e31a681c9a170b8d837e
ton that hoi <tonthathoi.090302@gmail.com>
2022-11-24 18:39:10 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="App">
-      <div className="text-[red]">hello word</div>
+      <div className="text-[red]">hello word kahjsdkjhasjkdjkas</div>
     </div>
   );
 }
